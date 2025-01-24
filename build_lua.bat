64b70112f66144c8e5a58b20e45c77f317f57c37
DoComplement <92898998+DoComplement@users.noreply.github.com>
2025-01-24 13:11:41 -0500

    Update build_lua.bat

--- build_lua.bat
+++ build_lua.bat
@@ -1,14 +1,18 @@
 @echo off
 
-if not exist %1 (
+if "%1"=="" (
  echo error: no argument provided
  exit /B
-) else if %~x1 NEQ .lua (
- echo error: filetype is incorrect; %~x1 collected, .lua expected
+) else if not "%~x1"==".lua" (
+ echo error: filetype is incorrect; .lua expected, got %~x1
+ exit /B
+) else if not exist "%~f1" (
+ echo error: couldn't find "%~f1"
  exit /B
 )
 
 SET fname=%~n1.exe
 SET build=%USERPROFILE%\.personal\build_lua
 
-%build%\glue.exe %build%\srlua.exe %~f1 %~dp1\%fname%
+%build%\glue.exe %build%\srlua.exe "%~f1" "%~dp1%fname%"
+echo built "%~dp1%fname%"
