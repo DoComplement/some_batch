683c9320c6a6b3c2afd7cf8fa71dfd1ec719d8ba
DoComplement <92898998+DoComplement@users.noreply.github.com>
2025-04-12 20:06:47 -0400

    Update build_srlua.bat

--- build_srlua.bat
+++ build_srlua.bat
@@ -6,24 +6,23 @@ if not exist \MinGW\lua-5.4.7 (
 )
 
 cd %USERPROFILE%\
-if exist .personal (rmdir /S /Q .personal)
-if exist some_batch (rmdir /S /Q some_batch)
+if exist .\.personal (rmdir /S /Q .\.personal)
+if exist .\some_batch (rmdir /S /Q .\some_batch)
 
-git clone -q https://github.com/DoComplement/some_batch.git
-ren some_batch .personal && cd .\.personal
-if not exist build_lua (mkdir build_lua)
+git clone -q https://github.com/DoComplement/some_batch.git .personal
+cd .\.personal && mkdir build_lua
 
 git clone -q https://github.com/LuaDist/srlua
 
-del /Q srlua\Makefile
-move Makefile srlua
+del /Q .\srlua\Makefile
+move Makefile .\srlua
 
-cd srlua && make 
+cd .\srlua && make 
 move .\*.exe ..\build_lua
 
 cd ..
-rmdir /S /Q srlua,.git
-del /S /Q README.md
+rmdir /S /Q .\srlua,.\.git
+del /S /Q .\README.md
 
 
 echo All done! yay!
